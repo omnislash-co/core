fb32e21f8898595997b0e159ba41bd8bb04eeef7
Inderbir Ghuman <inderbir.s97@gmail.com>
2024-05-08 15:24:29 -0400

    Update FAQ

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -9,15 +9,22 @@
                 <div class="content">
                     <h4 class="color-accent">What is Omnislash?</h4>
                     <p>
-                        Omnislash is a platform that allows users to discover, track, review, recommend and discuss JRPG style video games.
+                        Omnislash is a platform that allows users to discover, track, review, recommend and discuss JRPG video games.
+                    </p>
+
+                    <h4 class="color-accent">Which games are added?</h4>
+                    <p>
+                        The primary focus is on JRPGs, role-playing games from Japan-based developers. Though games that are inspired by JRPGs, such as <em>Chained Echoes</em>, may be added to the database (this is a low priority).
                     </p>
 
                     <h4 class="color-accent">How are games consolidated?</h4>
                     <p>
-                        On some gaming websites, games generally have their own page/section even if it's the same game just on different platforms. In order to consolidate pages such as these, the following guidelines are used when adding games to the database.
-                        <blockquote>
-                            These guidelines are not set in stone and may change in the future. If you would like to provide some feedback regarding this process, please let us know via the <a href="{{ route('waterhole.home') }}">community forums</a> in the "ideas" channel!
-                        </blockquote>
+                        The following guidelines are used when consolidating games.
+                        <div class="alert bg-warning-soft text-sm">
+                            <p>
+                                These guidelines are not set in stone and may change in the future. If you would like to provide some feedback regarding this process, please let us know via the <a href="{{ route('waterhole.home') }}">community forums</a> in the "ideas" channel!
+                            </p>
+                        </div>
                     </p>
                     <p>
                         <h6>Shared Pages</h6>
@@ -25,7 +32,7 @@
                             <li>Multiplatform games</li>
                             <li>
                                 Ports/Remasters<br>
-                                <em class="text-xs">Example: Xenoblade Chronicles (Nintendo Wii) will be grouped together with Xenoblade Chronicles 3D (New Nintendo 3DS).</em>
+                                <em class="text-xs color-muted">Example: Xenoblade Chronicles (Nintendo Wii) will be grouped together with Xenoblade Chronicles 3D (New Nintendo 3DS).</em>
                             </li>
                         </ul>
                     </p>
@@ -34,10 +41,10 @@
                         <h6>Seperate Pages</h6>
                         <ul>
                             <li>Remakes<br>
-                                <em class="text-xs">Examples: Super Mario RPG (Switch), Final Fantasy III (Nintendo DS)</em>
+                                <em class="text-xs color-muted">Examples: Super Mario RPG (Switch), Final Fantasy III (Nintendo DS)</em>
                             </li>
-                            <li>Ports/Remasters that add new content (story, features etc.)<br>
-                                <em class="text-xs">Example: Xenoblade Chronicles: Definitive Edition</em>
+                            <li>Ports/Remasters that add new content (story, gameplay changes etc.)<br>
+                                <em class="text-xs color-muted">Example: Xenoblade Chronicles: Definitive Edition</em>
                             </li>
                         </ul>
                     </p>
