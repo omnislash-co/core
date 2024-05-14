0cba6c809eb7329811a84ed38b105cae30dfae19
Inderbir Ghuman <inderbir.s97@gmail.com>
2024-05-14 14:57:34 -0400

    Update footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -31,7 +31,6 @@
                         <li>
                             <a href="{{ route('waterhole.home') }}">Community</a>
                         </li>
-                        {{-- <li><a href="{{ route('waterhole.page', 'community-guide') }}">Guidelines</a></li> --}}
                         <li>
                             <a href="https://github.com/omnislash-co">GitHub</a>
                         </li>
