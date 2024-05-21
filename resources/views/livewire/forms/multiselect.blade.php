f2bf9e5c3a843cfc4e589d51349f8783fa979253
Inderbir Ghuman <inderbir.s97@gmail.com>
2024-05-20 20:22:42 -0400

    Add game filters

---
+++ resources/views/livewire/forms/multiselect.blade.php
@@ -0,0 +1,49 @@
+<div class="stack gap-sm">
+    <div style="position: relative">
+        <x-waterhole::field
+            :name="strtolower($label)"
+            :label="$label"
+        >
+            <div class="input-container">
+                @icon('tabler-search', ['class' => 'no-pointer'])
+                <input role="button" type="text" name="search" 
+                    wire:model.live.debounce="search" 
+                    wire:focus="toggleOptions(true)" 
+                    wire:blur="toggleOptions(false)">
+            </div>        
+        </x-waterhole::field>
+
+        @if ($showOptions)
+            <div class="menu filters-menu" wire:transition.in.origin.top>
+                @forelse ($this->filtered as $option)
+                    <button 
+                        @class([
+                            'menu-item',
+                            'full-width',
+                            'is-active' => $this->isSelected($option->id),
+                        ])
+                        role="menuitem"
+                        wire:click="add({{$option->id}})">
+                        {{ $option->name }}
+                        @icon('tabler-check', ['class' => 'menu-item__check'])
+                    </button>
+                @empty
+                    <div class="p-xs">No {{ strtolower($label) }} found.</div>
+                @endforelse
+            </div>
+        @endif
+    </div>
+
+    @if (count($selected) > 0)
+        <div class="row wrap gap-xs">
+            @foreach ($selected as $id)
+                <div class="badge bg-warning-soft" style="padding-right: 0">
+                    {{ $this->getOptionName($id) }}
+                    <button class="btn btn--icon btn--transparent btn--sm" wire:click="remove({{ $id }})">
+                        @icon('tabler-x')
+                    </button>
+                </div>
+            @endforeach
+        </div>
+    @endif
+</div>
--- resources/views/livewire/games/browse.blade.php
+++ resources/views/livewire/games/browse.blade.php
@@ -1,28 +1,85 @@
 <div class="stack gap-gutter">
     
-    <div class="stack gap-sm">
+    <div class="stack gap-sm" x-data="{ open: false }" x-init="{{ $this->hasFilters() }} ? open=true : '';">
         <div class="row gap-sm">
             <div class="input-container grow">
                 @icon('tabler-search', ['class' => 'no-pointer'])
                 <input type="text" placeholder="Search" wire:model.live.debounce="search">
             </div>
-            <button class="btn btn--narrow bg-accent" disabled>
+
+            <button class="btn btn--narrow bg-accent" @click="open = !open">
                 @icon('tabler-filter')
+                <span class="hide-md-down">
+                    Filters
+                </span>
             </button>
+
+            <ui-popup placement="bottom-end">
+                <button class="btn btn--narrow">
+                    @icon('tabler-sort-descending')
+                    <span class="hide-md-down">
+                        Sort
+                    </span>
+                </button>
+              
+                <ui-menu class="menu" hidden>
+                  <button @class(['menu-item', 'is-active' => $sort->name === 'POPULAR' ]) wire:click="setSort('POPULAR')" role="menuitem">
+                    @icon('tabler-heart')
+                    Popularity
+                  </button>
+                  <button @class(['menu-item', 'is-active' => $sort->name === 'SCORE' ]) wire:click="setSort('SCORE')" role="menuitem">
+                    @icon('tabler-star')
+                    Average Score
+                  </button>
+                  <button @class(['menu-item', 'is-active' => $sort->name === 'CREATED' ]) wire:click="setSort('CREATED')" role="menuitem">
+                    @icon('tabler-clock')
+                    Recently Added
+                  </button>
+                </ui-menu>
+              </ui-popup>
+        </div>
+
+        <div 
+            class="alert"
+            x-cloak 
+            x-transition 
+            x-show="open">
+            <div class="grid gap-sm full-width" style="--grid-min: 33ch;">
+                <livewire:forms.multiselect :key="1" 
+                    label="Developers" 
+                    :options="$this->getDevelopers"
+                    @update="updatQueryParams($event.detail.selected, 'developers')"
+                     />
+                <livewire:forms.multiselect :key="2" 
+                    label="Genres" 
+                    :options="$this->getGenres" 
+                    @update="updatQueryParams($event.detail.selected, 'genres')" />
+                <livewire:forms.multiselect :key="3" 
+                    label="Platforms" 
+                    :options="$this->getPlatforms" 
+                    @update="updatQueryParams($event.detail.selected, 'platforms')" />
+            </div>
         </div>
     </div>
 
-    @if (count($this->games)>0)
-        <div class="game-cards grid gap-md">
-            @foreach ($this->games as $game)
-                <x-cards.game :$game />
-            @endforeach
-        </div>
-        {{ $this->games->links() }}
-    @else
-        <div class="alert bg-warning-soft">
-            No games found.
-        </div>
-    @endif
+    <div wire:loading.delay> 
+        <x-waterhole::spinner class="spinner--block"/>
+    </div>
 
+    <div wire:loading.delay.remove>
+        @if (count($this->games)>0)
+            <div class="game-cards grid gap-md">
+                @foreach ($this->games as $game)
+                    <x-cards.game :$game wire:key="{{ $game->id }}" />
+                @endforeach
+            </div>
+            <div style="margin-top: var(--space-md)">
+                {{ $this->games->links() }}
+            </div>
+        @else
+            <div class="alert bg-warning-soft">
+                No games found.
+            </div>
+        @endif
+    </div>
 </div>
