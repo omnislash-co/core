ef461b60a17c59836eb30354d2ac9baab6417407
Inderbir Ghuman <inderbir.s97@gmail.com>
2025-09-22 15:18:32 -0400

    Update Library modal

--- resources/views/games/library.blade.php
+++ resources/views/games/library.blade.php
@@ -2,49 +2,71 @@
 
     <div class="stack gap-lg">
         @if (count($libraries) > 0)
-            <div class="table-container full-width">
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th>Platform</th>
-                            <th>Play Status</th>
-                            <th>Score</th>
-                            <th>Hours Played</th>
-                            <th>Notes</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($libraries as $entry)
-                            <tr>
-                                <td>{{ $entry->platform->acronym }}</td>
-                                <td>
-                                    <span class="badge {{ $entry->playStatus->css_class }}">{{ $entry->playStatus->name }}</span>
-                                </td>
-                                <td>{{ $entry->score ? $entry->score : '-' }}</td>
-                                <td>{{ $entry->hours ? $entry->hours : '-' }}</td>
-                                <td>
-                                    @if ($entry->notes)
-                                        <button class="btn">
-                                            @icon('tabler-notes')
-                                            <ui-tooltip>{{ $entry->notes }}</ui-tooltip>
-                                        </button>
-                                    @else
-                                    -
-                                    @endif
-                                </td>
-                                <td class="row gap-xxs justify-end">
-                                    <a href="{{ route('library.edit', ['game' => $game->slug, 'library' => $entry->id]) }}" class="btn">@icon('tabler-edit')</a>
-                                    <form action="{{ route('library.destroy', ['game' => $game->slug, 'library' => $entry->id]) }}" method="POST" onsubmit="return confirm('Are you sure you want to delete this entry?');">
-                                        @method('DELETE')
-                                        @csrf
-                                        <button class="btn bg-danger" type="submit">@icon('tabler-trash')</button>
-                                    </form>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+            <div class="text-xs">
+                <div class="row">
+                    <div class="p-sm weight-bold" style="flex: 1">Platform</div>
+                    <div class="p-sm weight-bold hide-md-down" style="flex: 1">Play Status</div>
+                    <div class="p-sm weight-bold text-center hide-md-down" style="flex: 0.5">Score</div>
+                    <div class="p-sm weight-bold text-center hide-md-down" style="flex: 0.5">Hours</div>
+                    <div class="p-sm weight-bold text-center hide-md-down" style="flex: 0.5">Notes</div>
+                    <div class="p-sm weight-bold hide-md-down" style="flex: 1.5"></div>
+                </div>
+                @foreach ($libraries as $entry)
+                    <div class="row align-center" style="border-top: 1px solid var(--color-fill);">
+                        <div class="p-sm stack gap-xxs" style="flex: 1">
+                            <div class="row wrap gap-xxs">
+                                <span>{{ $entry->platform->acronym }}</span>
+                                <span class="badge hide-lg-up {{ $entry->playStatus->css_class }}">{{ $entry->playStatus->name }}</span>
+                            </div>
+                            <div class="row wrap gap-xxs hide-lg-up">
+                                @if ($entry->score)
+                                    <span class="badge">
+                                        @icon('tabler-heart')
+                                        {{ $entry->score }}
+                                    </span>
+                                @endif
+                                @if ($entry->hours)
+                                    <span class="badge">
+                                        @icon('tabler-clock')
+                                        {{ $entry->hours.'hrs' }}
+                                    </span>
+                                @endif
+                            </div>
+                            <div>
+                                @if ($entry->notes)
+                                    <span class="badge hide-lg-up">
+                                        @icon('tabler-notes')
+                                        Notes
+                                        <ui-tooltip>{{ $entry->notes }}</ui-tooltip>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="p-sm hide-md-down" style="flex: 1">
+                            <span class="badge {{ $entry->playStatus->css_class }}">{{ $entry->playStatus->name }}</span>
+                        </div>
+                        <div class="p-sm text-center hide-md-down" style="flex: 0.5">{{ $entry->score ? $entry->score : '-' }}</div>
+                        <div class="p-sm text-center hide-md-down" style="flex: 0.5">{{ $entry->hours ? $entry->hours : '-' }}</div>
+                        <div class="p-sm text-center hide-md-down" style="flex: 0.5">
+                            @if ($entry->notes)
+                                <button class="btn">
+                                    @icon('tabler-notes')
+                                    <ui-tooltip>{{ $entry->notes }}</ui-tooltip>
+                                </button>
+                            @else
+                                <span>-</span>
+                            @endif
+                        </div>
+                        <div class="p-sm row wrap gap-xxs justify-end" style="flex: 1.5">
+                            <a href="{{ route('library.edit', ['game' => $game->slug, 'library' => $entry->id]) }}" class="btn">@icon('tabler-edit')</a>
+                            <form action="{{ route('library.destroy', ['game' => $game->slug, 'library' => $entry->id]) }}" method="POST" onsubmit="return confirm('Are you sure you want to delete this entry?');">
+                                @method('DELETE')
+                                @csrf
+                                <button class="btn bg-danger" type="submit">@icon('tabler-trash')</button>
+                            </form>
+                        </div>
+                    </div>
+                @endforeach
             </div>
 
             <div class="row gap-sm">
@@ -61,5 +83,5 @@
             </div>
         @endif
     </div>
-    
+
 </x-modals.library>
